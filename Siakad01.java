9e209ff7eb32b98abf52715575ff676267a93114
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2024-09-12 14:44:34 +0700

    modified:   Siakad01.class 	modified:   Siakad01.java

--- Siakad01.java
+++ Siakad01.java
@@ -3,24 +3,27 @@ public class Siakad01{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nama, nim;
-        char kelas;
+        String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis , nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
         System.out.print("Masukkan nim: ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan absen: ");
         absen = sc.nextByte();
         System.out.print("Masukkan nilai kuis: ");
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian ) / 3;
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis*20/100) + (nilaiTugas*15/100) + (nilaiUAS*30/100) +(nilaiUTS*35/100 );
+
         System.out.println("Nama\t\t: " + nama );
         System.out.println("NIM\t\t: " +nim);
         System.out.println("Kelas\t\t: " + kelas);
