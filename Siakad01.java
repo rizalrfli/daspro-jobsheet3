29fd0a6a14993860185316b7ab3600ac44db4984
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2024-09-12 12:55:30 +0700

    new file:   Siakad01.class new file:   Siakad01.java

---
+++ Siakad01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Siakad01{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        System.out.println("Masukkan nama: ");
+        nama = sc.nextLine();
+        System.out.println("Masukkan nim: ");
+        nim = sc.nextLine();
+        System.out.println("Masukkan kelas: ");
+        kelas = sc.nextLine().charAt(0);
+        System.out.println("Masukkan absen: ");
+        absen = sc.nextByte();
+        System.out.println("Masukkan nilai kuis: ");
+        nilaiKuis = sc.nextDouble();
+        System.out.println("Masukkan nilai tugas: ");
+        nilaiTugas = sc.nextDouble();
+        System.out.println("Masukkan nilai ujian: ");
+        nilaiUjian = sc.nextDouble();
+        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian ) / 3;
+        System.out.println("Nama: " + nama + "NIM: " +nim);
+        System.out.println("Kelas: " + kelas + "Absen: " + absen);
+        System.out.println("Nilai akhir: " + nilaiAkhir);
+    }
+}
